a343ee66b36e10e3e94669ed3d6e2c107f2a61b8
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-10 22:41:29 +0000

    use profile model

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,18 @@ from django.db import models
 class UserProfile(models.Model):
 
     id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        db_column="user_id"
+    )
+
     phone = models.CharField(max_length=20, null=True)
+
     currency = models.CharField(max_length=10, default="EUR")
+
+    created_at = models.DateTimeField()
 
     class Meta:
         managed = False

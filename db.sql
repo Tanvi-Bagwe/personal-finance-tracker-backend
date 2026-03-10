83e751621924fad6ea74314bfa2324f2c11ab9df
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-10 22:42:07 +0000

    db schema

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
-CREATE TABLE personal_finance_tracker.user_profiles (
+CREATE TABLE finance.user_profiles (
     id SERIAL PRIMARY KEY,
     user_id INT UNIQUE,
     phone VARCHAR(20),
-    currency VARCHAR(10),
+    currency VARCHAR(10) DEFAULT 'EUR',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

a1001a5d2c76503534a45976e61e492aeb5ac4a4
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-10 22:21:30 +0000

    new model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, null=True)
+    currency = models.CharField(max_length=10, default="EUR")
+
+    class Meta:
+        managed = False
+        db_table = "user_profiles"

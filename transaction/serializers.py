9d1bfcc3213838596705c1a51703a7e3196fa0ab
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-21 12:57:56 +0000

    serializers created for transaction

---
+++ transaction/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from category.constant import CategoryFields
+from .constant import TransactionFields
+from .models import Transaction
+
+
+class CreateTransactionSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(max_digits=12, decimal_places=2, min_value=0.01)
+    category_id = serializers.IntegerField()
+    type = serializers.ChoiceField(choices=[CategoryFields.INCOME, CategoryFields.EXPENSE])
+    description = serializers.CharField(max_length=255, required=False, allow_blank=True)
+    date = serializers.DateField()
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
+    class Meta:
+        model = Transaction
+        fields = [
+            TransactionFields.ID,
+            TransactionFields.AMOUNT,
+            TransactionFields.TYPE,
+            TransactionFields.CATEGORY,
+            TransactionFields.CATEGORY_NAME,
+            TransactionFields.DESCRIPTION,
+            TransactionFields.DATE
+        ]

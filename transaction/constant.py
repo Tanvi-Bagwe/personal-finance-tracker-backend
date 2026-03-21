9bb4b40f830404734f11a57430ceaf00cc6d2030
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-21 12:58:14 +0000

    constants created for transaction

---
+++ transaction/constant.py
@@ -0,0 +1,8 @@
+class TransactionFields:
+    ID = "id"
+    AMOUNT = "amount"
+    TYPE = "type"
+    CATEGORY = "category_id"
+    CATEGORY_NAME = "category_name"
+    DESCRIPTION = "description"
+    DATE = "date"

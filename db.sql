5a887d6c2c581f03dc22b109f7a9964a5ce08111
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-22 09:41:54 +0000

    schema updates for reminders

--- db.sql
+++ db.sql
@@ -27,3 +27,19 @@ CREATE TABLE personal_finance_tracker.transactions (
     FOREIGN KEY (user_id) REFERENCES personal_finance_tracker.auth_user(id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES personal_finance_tracker.categories(id) ON DELETE SET NULL
 );
+
+CREATE TABLE personal_finance_tracker.reminders (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    amount DECIMAL(12, 2),
+    due_date DATE NOT NULL,
+    reminder_days_before INT DEFAULT 1, -- How many days before to send email
+    is_read BOOLEAN DEFAULT FALSE,      -- For app notifications
+    is_completed BOOLEAN DEFAULT FALSE, -- To hide from "Pending" list
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_reminder_user FOREIGN KEY(user_id) REFERENCES personal_finance_tracker.auth_user(id) ON DELETE CASCADE
+);
+
+ALTER TABLE personal_finance_tracker.reminders
+ADD COLUMN last_notified_at DATE;

fa2bfe6f18edb1ac8312b5c249d83acfd59a9e22
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-22 09:43:43 +0000

    constants for reminder

---
+++ reminder/constant.py
@@ -0,0 +1,8 @@
+class ReminderFields:
+    ID = "id"
+    TITLE = "title"
+    AMOUNT = "amount"
+    DUE_DATE = "due_date"
+    REMINDER_DAYS = "reminder_days_before"
+    IS_READ = "is_read"
+    IS_COMPLETED = "is_completed"
